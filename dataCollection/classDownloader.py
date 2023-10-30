95cf7bd95cf62767e48c392182de6edeee613ad1
Ean Smith <eansmith@comcast.net>
2023-10-30 18:25:42 -0400

    updated CD

--- dataCollection/classDownloader.py
+++ dataCollection/classDownloader.py
@@ -4,7 +4,9 @@ import requests
 
 termNumber = 2244
 
-endPoint = "https://pitcsprd.csps.pitt.edu/psc/pitcsprd/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch"
+#endPoint = "https://pitcsprd.csps.pitt.edu/psc/pitcsprd/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch"
+
+endPoint = "https://pitcsprd.csps.pitt.edu/psc/pitcsprd/EMPLOYEE/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UPITT&term=2244&date_from=&date_thru=&subject=&subject_like=&catalog_nbr=&time_range=&days=&campus=PIT&location=&x_acad_career=UGRD&acad_group=&rqmnt_designtn=&instruction_mode=&keyword=&class_nbr=&acad_org=&enrl_stat=&crse_attr=&crse_attr_value=&instructor_name=&session_code=&units=&page=10000"
 
 data = {'institution' : 'UPITT' ,
         'term' : termNumber,
@@ -32,3 +34,8 @@ data = {'institution' : 'UPITT' ,
         'units' : None,
         'page' : '1'}
 
+#r = requests.get(url=endPoint, data=data)
+r = requests.get(url=endPoint)
+
+print(len(r.content))
+#print(r.text)
