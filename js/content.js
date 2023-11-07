7891c2662d0a8eba2feab1ce735f7826307f7073
Ean Smith <eansmith@comcast.net>
2023-11-06 21:34:59 -0500

    added mutationObserver

--- js/content.js
+++ js/content.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+function handleSomeDiv(someDiv) { 
+    console.log("div was handled");
+    console.log(someDiv);
+}
+
+const observer = new MutationObserver(function (mutations) {
+    const someDiv = document.getElementsByClassName("MuiGrid-root MuiGrid-container MuiGrid-wrap-xs-nowrap MuiGrid-align-items-xs-center");
+    if (someDiv) {
+        handleSomeDiv(someDiv);
+    }
+});
+
+
+observer.observe(document, {
+    childList: true,
+    subtree:   true
+});
