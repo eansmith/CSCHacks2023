260962d7f7f79275c18eb4e88c837beafc31ca3c
Ean Smith <eansmith@comcast.net>
2023-11-06 22:13:37 -0500

    gets all prof names

--- js/content.js
+++ js/content.js
@@ -3,10 +3,30 @@ function handleSomeDiv(someDiv) {
     console.log(someDiv);
 }
 
+function gridToProf(element){
+    //1 0 0 0 3 0 0 0 4 0
+    //profElement = element.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[3].childNodes[0].childNodes[0].childNodes[0].childNodes[4].childNodes[0];
+    profElement = element.getElementsByClassName("MuiGrid-root MuiGrid-item MuiGrid-zeroMinWidth MuiGrid-grid-xs-12").item(0);
+
+    profElement = profElement.childNodes[0].childNodes[0];
+    console.log(profElement.textContent);
+}
 const observer = new MutationObserver(function (mutations) {
-    const someDiv = document.getElementsByClassName("MuiGrid-root MuiGrid-container MuiGrid-wrap-xs-nowrap MuiGrid-align-items-xs-center");
-    if (someDiv) {
-        handleSomeDiv(someDiv);
+    const elements = document.getElementsByClassName("MuiGrid-root MuiGrid-container MuiGrid-wrap-xs-nowrap MuiGrid-align-items-xs-center");
+
+    for (var i = 0; i < elements.length; i++) {
+        if(elements.item(i).childNodes.length < 2){
+            continue;
+        }
+        if(elements.item(i).childNodes[1].className === "MuiGrid-root MuiGrid-item"){
+            continue;
+        }
+        //console.log(elements.item(i));
+        gridToProf(elements.item(i));
+    }
+
+    if (elements) {
+        handleSomeDiv(elements);
     }
 });
 
