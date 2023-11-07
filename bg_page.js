f3477ef49c694e5d94b3e8b0bfffcbd3cd0af288
Ean Smith <eansmith@comcast.net>
2023-11-07 16:12:53 -0500

    bg page no proxy init vesion

---
+++ bg_page.js
@@ -0,0 +1,23 @@
+chrome.runtime.onMessage.addListener(
+    function(url, sender, onSuccess) {
+            //to-do add subject to pick the correct professor adam lee has two
+        fetch("https://www.ratemyprofessors.com/graphql", {
+        //const re = await fetch("https://corsproxy.io/?https://www.ratemyprofessors.com/graphql", {
+                               "headers": {
+                               "accept": "*/*", 
+                               "accept-language": "en-US,en;q=0.9",
+                               "authorization": "Basic dGVzdDp0ZXN0",
+                               "content-type": "application/json",
+                               "sec-fetch-mode": "cors",
+                               "sec-fetch-site": "same-origin"
+                               },
+                               "referrerPolicy": "strict-origin-when-cross-origin",
+                               "body": "{\"query\":\"query TeacherSearchResultsPageQuery(\\n  $query: TeacherSearchQuery!\\n  $schoolID: ID\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1ZLmLD\\n  }\\n  school: node(id: $schoolID) {\\n    __typename\\n    ... on School {\\n      name\\n    }\\n    id\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\\n  teachers(query: $query, first: 8, after: \\\"\\\") {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n\",\"variables\":{\"query\":{\"text\":\"\",\"schoolID\":\"U2Nob29sLTEyNDc=\",\"fallback\":true,\"departmentID\":null},\"schoolID\":\"U2Nob29sLTEyNDc=\"}}",
+                               "method": "POST",
+                               "mode": "cors"
+                               }).then(response => response.text()).then(r => console.log(r))
+                               .then(responseText => onSuccess(responseText))
+        
+        return true;  // Will respond asynchronously.
+    }
+);
--- js/content.js
+++ js/content.js
@@ -43,13 +43,13 @@ async function proxyGetRMP(name){
                            "accept-language": "en-US,en;q=0.9",
                            "authorization": "Basic dGVzdDp0ZXN0",
                            "content-type": "application/json",
-                           "sec-fetch-mode": "cors",
+                           "sec-fetch-mode": "no-cors",
                            "sec-fetch-site": "same-origin"
                            },
                            "referrerPolicy": "strict-origin-when-cross-origin",
                            "body": "{\"query\":\"query TeacherSearchResultsPageQuery(\\n  $query: TeacherSearchQuery!\\n  $schoolID: ID\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1ZLmLD\\n  }\\n  school: node(id: $schoolID) {\\n    __typename\\n    ... on School {\\n      name\\n    }\\n    id\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\\n  teachers(query: $query, first: 8, after: \\\"\\\") {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n\",\"variables\":{\"query\":{\"text\":\""+ name + "\",\"schoolID\":\"U2Nob29sLTEyNDc=\",\"fallback\":true,\"departmentID\":null},\"schoolID\":\"U2Nob29sLTEyNDc=\"}}",
                            "method": "POST",
-                           "mode": "cors"
+                           "mode": "no-cors"
                            });
     
     const data = await re.json();
@@ -135,7 +135,7 @@ const observer = new MutationObserver(function (mutations) {
         changeProfName(entry, ratingDict[gridToProf(entry)]);
     }*/
 
-    //promise all method
+   /* //promise all method
     promises = []
     for (const prof of profs){
         promises.push(proxyGetRMP(prof))
@@ -155,7 +155,9 @@ const observer = new MutationObserver(function (mutations) {
         for (const entry of grid){
             changeProfName(entry, ratingDict[gridToProf(entry)]);
         }
-    });
+    });*/
+
+    
     
 
     
@@ -163,7 +165,10 @@ const observer = new MutationObserver(function (mutations) {
     console.log("mutObs Disconnected");
     observer.disconnect();
 });
-
+chrome.runtime.sendMessage( 
+    "",//goes to bg_page.js
+      data => console.log(data) //your callback
+    ); 
 // send list of unique prof Return dict with rating
 observer.observe(document, {
     childList: true,
