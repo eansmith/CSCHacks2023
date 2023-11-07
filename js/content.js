264a88709ec151a23a2e11783cb55843f1a21002
Ean Smith <eansmith@comcast.net>
2023-11-07 09:37:02 -0500

    organized content.js

--- js/content.js
+++ js/content.js
@@ -1,36 +1,35 @@
-function handleSomeDiv(someDiv) { 
-    console.log("div was handled");
-    console.log(someDiv);
-}
-
 function gridToProf(element){
-    //1 0 0 0 3 0 0 0 4 0
-    //profElement = element.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[3].childNodes[0].childNodes[0].childNodes[0].childNodes[4].childNodes[0];
     profElement = element.getElementsByClassName("MuiGrid-root MuiGrid-item MuiGrid-zeroMinWidth MuiGrid-grid-xs-12").item(0);
 
     profElement = profElement.childNodes[0].childNodes[0];
-    console.log(profElement.textContent);
+    profName = profElement.textContent.replace(/[^a-z0-9\s]/gi, '');
+    console.log(profName);
 }
+//https://stackoverflow.com/questions/60337528/chrome-extension-cross-origin-requests-in-background-script-blocked
+//https://stackoverflow.com/questions/12357485/cors-chrome-extension-with-manifest-version-2
 
-async function getRMP(name){
+//corproxy.io could be unsafe and should not be used
+async function proxyGetRMP(name){
+    //to-do add subject to pick the correct professor adam lee has two
     const re = await fetch("https://corsproxy.io/?https://www.ratemyprofessors.com/graphql", {
-  "headers": {
-    "accept": "*/*", 
-    "accept-language": "en-US,en;q=0.9",
-    "authorization": "Basic dGVzdDp0ZXN0",
-    "content-type": "application/json",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin"
-  },
-  "referrerPolicy": "strict-origin-when-cross-origin",
-  "body": "{\"query\":\"query TeacherSearchResultsPageQuery(\\n  $query: TeacherSearchQuery!\\n  $schoolID: ID\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1ZLmLD\\n  }\\n  school: node(id: $schoolID) {\\n    __typename\\n    ... on School {\\n      name\\n    }\\n    id\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\\n  teachers(query: $query, first: 8, after: \\\"\\\") {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n\",\"variables\":{\"query\":{\"text\":\"william garrison\",\"schoolID\":\"U2Nob29sLTEyNDc=\",\"fallback\":true,\"departmentID\":null},\"schoolID\":\"U2Nob29sLTEyNDc=\"}}",
-  "method": "POST",
-  "mode": "cors"
-    });
+                           "headers": {
+                           "accept": "*/*", 
+                           "accept-language": "en-US,en;q=0.9",
+                           "authorization": "Basic dGVzdDp0ZXN0",
+                           "content-type": "application/json",
+                           "sec-fetch-mode": "cors",
+                           "sec-fetch-site": "same-origin"
+                           },
+                           "referrerPolicy": "strict-origin-when-cross-origin",
+                           "body": "{\"query\":\"query TeacherSearchResultsPageQuery(\\n  $query: TeacherSearchQuery!\\n  $schoolID: ID\\n) {\\n  search: newSearch {\\n    ...TeacherSearchPagination_search_1ZLmLD\\n  }\\n  school: node(id: $schoolID) {\\n    __typename\\n    ... on School {\\n      name\\n    }\\n    id\\n  }\\n}\\n\\nfragment TeacherSearchPagination_search_1ZLmLD on newSearch {\\n  teachers(query: $query, first: 8, after: \\\"\\\") {\\n    didFallback\\n    edges {\\n      cursor\\n      node {\\n        ...TeacherCard_teacher\\n        id\\n        __typename\\n      }\\n    }\\n    pageInfo {\\n      hasNextPage\\n      endCursor\\n    }\\n    resultCount\\n    filters {\\n      field\\n      options {\\n        value\\n        id\\n      }\\n    }\\n  }\\n}\\n\\nfragment TeacherCard_teacher on Teacher {\\n  id\\n  legacyId\\n  avgRating\\n  numRatings\\n  ...CardFeedback_teacher\\n  ...CardSchool_teacher\\n  ...CardName_teacher\\n  ...TeacherBookmark_teacher\\n}\\n\\nfragment CardFeedback_teacher on Teacher {\\n  wouldTakeAgainPercent\\n  avgDifficulty\\n}\\n\\nfragment CardSchool_teacher on Teacher {\\n  department\\n  school {\\n    name\\n    id\\n  }\\n}\\n\\nfragment CardName_teacher on Teacher {\\n  firstName\\n  lastName\\n}\\n\\nfragment TeacherBookmark_teacher on Teacher {\\n  id\\n  isSaved\\n}\\n\",\"variables\":{\"query\":{\"text\":\""+ name + "\",\"schoolID\":\"U2Nob29sLTEyNDc=\",\"fallback\":true,\"departmentID\":null},\"schoolID\":\"U2Nob29sLTEyNDc=\"}}",
+                           "method": "POST",
+                           "mode": "cors"
+                           });
+
     const data = await re.json();
     console.log(data);
 }
-getRMP("brob");
+proxyGetRMP("adam lee");
 
 const observer = new MutationObserver(function (mutations) {
     const elements = document.getElementsByClassName("MuiGrid-root MuiGrid-container MuiGrid-wrap-xs-nowrap MuiGrid-align-items-xs-center");
@@ -44,10 +43,6 @@ const observer = new MutationObserver(function (mutations) {
         }
         //console.log(elements.item(i));
         gridToProf(elements.item(i));
-    }
-
-    if (elements) {
-        handleSomeDiv(elements);
     }
 });
 
